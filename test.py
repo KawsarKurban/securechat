2212926f5b42994d3e279fd0ed7d636a4499f5c7
KawsarKurban <34701968+KawsarKurban@users.noreply.github.com>
2018-04-27 09:36:42 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,47 @@
+import socket
+from Crypto.PublicKey import RSA
+from Crypto import Random
+
+random_generator = Random.new().read
+private_key = RSA.generate(4096, random_generator)
+public_key = private_key.publickey()
+
+s = socket.socket()
+host = "10.0.42.182"
+port = 4032
+encrypt_str = "encrypted_message="
+
+if host == "127.0.0.1":
+    import commands
+    host = commands.getoutput("Hostname -I")
+print "host = " + host
+
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s.bind((host, port))
+
+s.listen(5)
+
+c, addr= s.accept()
+
+while True:
+    data = c.recv(1024)
+    data = data.replace("\r\n", '')
+
+    if data == "Client: OK":
+        c.send("public_key=" + public_key.exportKey() + "\n")
+        print "Public key sent to client."
+    
+    elif encrypt_str in data:
+        data = data.replace(encrypt_str, '')
+        print "Received:\nEncrypted message = "+str(data)
+        encrypted = eval(data)
+        decrypted = private_key.decrypt(encrypted)
+        c.send("Server: OK")
+        print "Decrypted message = " + decrypted
+    
+    elif data == "Quit": break
+
+c.send("Server stopped\n")
+print "Server stopped"
+c.close()
